6aefcb284a8b8fc1221a7202a150539a47af2543
Agnes <agnes.mccall5@gmail.com>
2024-07-10 21:10:58 -0400

    add fakeDb.js and get /items

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const items = require('./fakeDb');
 
 const app = express();
 
 
-
+app.get('/items', (req, res) => {
+    res.json(items);
+});
 
 app.listen(3000, () => {
     console.log('listening on port 3000!');
